8dd1e6d022cf63f8a30d7c4a1910939337ea07ea
mdnozdl <medineozdil94@gmail.com>
2024-03-13 23:22:37 +0300

    TC_201

--- src/US_201_Register/TC_20101.java
+++ src/US_201_Register/TC_20101.java
@@ -1,34 +1,32 @@
-package US_202_Register_Negative;
+package US_201_Register;
 
-import Utlity.Tools;
-import org.testng.annotations.Test;
 import Utlity.BaseDriver;
-import org.testng.Assert;
+import Utlity.Tools;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 
-public class TC_20201_Negative extends BaseDriver {
-
+public class TC_20101 extends BaseDriver {
 
 
     @Test
-    public void TC_20201() {
-
+    public void TC_20101() {
         driver.get("https://demowebshop.tricentis.com/register");
-        Tools.Bekle(2);
+        Tools.Bekle(3);
 
         //WebElement register = driver.findElement(By.linkText("Register"));
         //register.click();
-        //Kişisel Bilgiler
+
+        // Kişisel Bilgiler
         WebElement cinsiyet = driver.findElement(By.xpath("//input[@id='gender-female']"));
         cinsiyet.click();
 
         WebElement ad = driver.findElement(By.xpath("//input[@id='FirstName']"));
-        ad.sendKeys("Mary");
+        ad.sendKeys("Team");
 
         WebElement soyad = driver.findElement(By.xpath("//input[@id='LastName']"));
-        soyad.sendKeys("Johnson");
+        soyad.sendKeys("Oniki");
 
         WebElement email = driver.findElement(By.xpath("//input[@id='Email']"));
         email.sendKeys("techno.team12@gmail.com");
@@ -40,19 +38,17 @@ public class TC_20201_Negative extends BaseDriver {
         sifreOnay.sendKeys("Pass1234");
 
         // Kayıt butonuna tıkla
-        WebElement kayitButon = driver.findElement(By.xpath("//input[@id='register-button']"));
-        kayitButon.click();
-
-        // Bekle ve hata mesajını kontrol et
-        Tools.Bekle(1);
-        WebElement hataMesaji = driver.findElement(By.xpath("//div[@class='validation-summary-errors']/ul/li"));
-        Assert.assertTrue(hataMesaji.getText().contains("The specified email already exists"), "Hata mesajı doğru görüntülenmedi.");
+        WebElement kayitOlButonu = driver.findElement(By.xpath("//input[@id='register-button']"));
+        kayitOlButonu.click();
 
         // WebDriver'ı kapat
-        Tools.Bekle(3);
+        Tools.Bekle(2);
         driver.quit();
     }
 
+
+
+
+
+
 }
-
-
